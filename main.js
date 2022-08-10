839942d3537e4e28c78b865396fb6fbddb78f30a
BlazeRavager1 <89845937+BlazeRavager1@users.noreply.github.com>
2022-08-10 17:43:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,13 +11,29 @@ Webcam.attach("#camera");
 
 function take_snapshot() {
   Webcam.snap(function (data_uri) {
-    document.getElementById("result").innerHTML ='<img id="captured_image" src = "'+data_uri+'"/>';
+    document.getElementById("result").innerHTML =
+      '<img id="captured_image" src = "' + data_uri + '"/>';
   });
 }
 
 console.log("ml5 version:", ml5.version);
-classifier = ml5.imageClassifier('teachablemachine.withgoogle.com/models/liLQNSDc0/model.json', modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/liLQNSDc0/model.json", modelLoaded);
 
 function modelLoaded() {
-    console.log("Model has been Loaded!");
+  console.log("Model has been Loaded!");
+}
+
+function check() {
+  img = document.getElementById("captured_image");
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
 }
